0b3b996aa3361f6d78fcbace8fd871f7e854529b
Vagner Barros <vagnerbarrospereira@gmail.com>
2019-06-26 09:32:27 -0300

    initial commit

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,6 +3,8 @@
 describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+    cy.contains('span', 'Vuetify')
+    cy.contains('span', 'MATERIAL DESIGN')
+    cy.contains('span', 'Latest Release')
   })
 })
