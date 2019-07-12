83dc7e6eb0d892a6492d11bcbb19f78744028674
Vagner Barros <vagnerbarrospereira@gmail.com>
2019-07-12 11:38:19 -0300

    .

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,6 @@ import './plugins/vuetify';
 import App from './App.vue';
 import router from './router/router';
 import store from './store/store';
-import './registerServiceWorker';
 import 'font-awesome/css/font-awesome.css';
 
 Vue.config.productionTip = false
