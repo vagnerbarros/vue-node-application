e48fbb19400cd90e51c5c857bd7cee606b3f6ddb
Vagner Barros <vagnerbarrospereira@gmail.com>
2019-10-03 10:54:54 -0300

    .

--- frontend/src/util/config.js
+++ frontend/src/util/config.js
@@ -2,10 +2,10 @@ let URL_ANRON_API;
 const TIMEOUT = 30000;
 
 if(process.env.NODE_ENV === 'production'){
-  URL_ANRON_API = '0.0.0.0:7000';
+  URL_ANRON_API = 'http://18.231.183.249:7000';
 }
 else{
-  URL_ANRON_API = 'http://127.0.0.1:7000';
+  URL_ANRON_API = 'http://18.231.183.249:7000';
 }
 
 module.exports = {
