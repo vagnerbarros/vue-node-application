d938b23b1ecd48aa48dba58aa9ae57e901a7e225
Vagner Barros <vagnerbarrospereira@gmail.com>
2019-10-03 10:51:36 -0300

    Fix local host

--- frontend/src/util/config.js
+++ frontend/src/util/config.js
@@ -2,7 +2,7 @@ let URL_ANRON_API;
 const TIMEOUT = 30000;
 
 if(process.env.NODE_ENV === 'production'){
-  URL_ANRON_API = 'http://127.0.0.1:7000';
+  URL_ANRON_API = '0.0.0.0:7000';
 }
 else{
   URL_ANRON_API = 'http://127.0.0.1:7000';
